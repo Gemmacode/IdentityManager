d036c1c5529b3ffe35d245109713187b4c4b89be
Gemma <jemmimah.abdul@decagon.dev>
2023-12-26 21:06:45 +0100

    Updated Project

---
+++ IdentityManager.Api/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using IdentityManager.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityManager.Api.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Register()
+        {
+            RegisterViewModel registerViewModel = new RegisterViewModel();
+            return View(registerViewModel);
+        }
+    }
+}
